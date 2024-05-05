6d539969751c9c25d13b3751c620434850c6b1a4
maitzeth <andre801mz@gmail.com>
2024-05-05 19:09:40 -0300

    refactor: :recycle: refactoring navbar to handle routes easily

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,29 +1,20 @@
 import { ROUTES } from '@/routes/routes';
 import { Link } from '@/components';
 
-const excludeRoutes = [ROUTES.modemsDetails, ROUTES.modems];
-
-const navbarLabelMap = {
-  modemsCreate: 'Create Modem',
-} as const;
-
 export const Navbar = () => {
   return (
     <nav>
       <ul className="flex items-center gap-2">
-        {Object.entries(ROUTES).map(([key, value], index) => {
-          if (excludeRoutes.includes(value)) {
-            return null;
-          }
-
-          return (
-            <li key={`navbar-item-${index}`} id={`navbar-item-${index}`}>
-              <Link to={value} className="px-4 py-2 rounded-md text-black hover:text-red-500 font-bold text-lg uppercase">
-                {navbarLabelMap[key as keyof typeof navbarLabelMap] ?? key}
-              </Link>
-            </li>
-          );
-        })}
+        <li>
+          <Link to={ROUTES.home} className="px-4 py-2 rounded-md text-black hover:text-red-500 font-bold text-lg uppercase">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to={ROUTES.modemsCreate} className="px-4 py-2 rounded-md text-black hover:text-red-500 font-bold text-lg uppercase">
+            Create
+          </Link>
+        </li>
       </ul>
     </nav>
   )
